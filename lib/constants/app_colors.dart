22cf98abb24688b635d2050c6699d081144d934c
Dmitrii <mr.tekeev14@gmail.com>
2024-06-24 01:26:09 +0600

    Add existing project files to Git

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color darkBlue = Color(0xff0A114F);
+  static const Color malina = Color(0xffF72055);
+  static const Color black = Color(0xff1D1D1D);
+  static const Color green = Color(0xff00B866);
+  static const Color softGreyBlue = Color(0xffF8F8F8);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color bgSoftGreyBlue = Color(0xffF8F8F9);
+  static const Color grey = Color(0xffDBDBDB);
+  static const Color darkGrey = Color(0xff828282);
+  static const Color red = Color(0xffE32727);
+  static const Color softGreyBlue2 = Color(0xffF5F5F6);
+  static const Color blue = Color(0xff2899EB);
+  static const Color orange = Color(0xffFFDF94);
+  static const Color pink = Color(0xffFFDEDD);
+}
---
+++ lib/constants/app_dimens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppDimens {
+  static const double smallIndent = 14;
+  static const double mediumIndent = 20;
+  static const double bigIndent = 30;
+  static const double mainPadding = 20;
+  static const double inputHeight = 60;
+  static const Radius inputCircularBorderRadius = Radius.circular(12);
+}
